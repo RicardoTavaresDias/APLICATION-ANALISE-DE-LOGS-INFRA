3ae69a563885848cb98b2cc020033e7d23f2f12d
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-09-08 19:34:58 -0300

    version 10.0 - create aplication

---
+++ main.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow } = require("electron")
+
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 920,
+    resizable: false,
+    webPreferences: {
+      nodeIntegration: true
+    },
+
+    autoHideMenuBar: true,
+    //frame: false
+  })
+
+  mainWindow.loadFile("index.html")
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform != 'darwin') {
+    app.quit()
+  }
+})
+
+
+
+
+
+/*
+    width: 1000,
+    height: 720,
+
+    width: 1000,
+    height: 675,
+*/
